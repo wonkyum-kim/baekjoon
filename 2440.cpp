4bf93060faf7da5ac081d29f9a6c76ab326a12c1
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-07-26 00:24:16 +0900

    별찍기 3

---
+++ 2440.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+	int n = 0;
+	std::cin >> n;
+
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = n; j > i; --j)
+			std::cout << '*';
+		std::cout << '\n';
+	}
+	return 0;
+}
