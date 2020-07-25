3e955d072c2d7216c9b795c0b1312a6c9b123fd5
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-07-25 23:48:58 +0900

    내림차순 정렬, 자릿수 구하기

--- 1427.cpp
+++ 1427.cpp
@@ -9,14 +9,16 @@ int main()
 	std::cin >> num;
 
 	std::vector<int> a;
-
+	
+	// 자릿수 구하기
 	while (num>0)
 	{
 		int x = num % 10;
 		a.push_back(x);
 		num /= 10;
 	}
-
+	
+	// 내림차순 
 	std::sort(a.begin(), a.end(), std::greater<int>());
 	for (auto& x : a)
 		std::cout << x;
