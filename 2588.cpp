ea005363d6f627ca80fd52a27f4ffbd1f8ce9007
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-07-26 00:12:57 +0900

    Create 2588.cpp

---
+++ 2588.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+	auto a = 0, b = 0;
+	std::cin >> a >> b;
+
+	int b100 = b / 100;
+	int b1 = b % 10;
+  int b10 = ((b - b1) / 10) % 10;
+
+	int x1 = a * b1;
+	int x2 = a * b10;
+	int x3 = a * b100;
+	int x4 = a * b;
+
+	cout << x1 << '\n' << x2 << '\n' << x3 << '\n' << x4;
+	return 0;
+}
