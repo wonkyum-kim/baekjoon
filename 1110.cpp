c236cb8b88b59f60e3a005d6e685d7fd2b696943
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-07-25 20:04:29 +0900

    Create 1110.cpp

---
+++ 1110.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	int a;
+	cin >> a;
+	std::vector<int> an;
+	an.push_back(a);
+
+	for (int i = 1;; ++i)
+	{
+    // a % 10 == 일의자리 숫자
+		a = (a % 10) * 10 + ((a % 10) + (a - a % 10) / 10) % 10;
+		if (a == an[0])
+		{
+			std::cout << i;
+			break;
+		}
+	}
+}
