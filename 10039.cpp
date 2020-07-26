89b89fbff7f0fe93b1a3971cd4aad53ad6a902c4
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-07-26 21:17:58 +0900

    Create 10039.cpp

---
+++ 10039.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	int sum = 0;
+	std::vector<int> score;
+
+	for (int i = 0; i < 5; i++)
+	{
+		int a;
+		std::cin >> a;
+		if (a < 40)
+			a = 40;
+		score.push_back(a);
+		sum += score[i];
+	}
+	std::cout << sum / 5;
+	return 0;
+}
