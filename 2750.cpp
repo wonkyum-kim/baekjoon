7136b494103b977eb4aa90d5ab0bbe8dc18f24d2
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-07-26 14:12:22 +0900

    Create 2750.cpp

---
+++ 2750.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+	int num;
+	std::cin >> num;
+
+	std::vector<int> a;
+	for (size_t i = 0; i < num; i++)
+	{
+		int x = 0;
+		std::cin >> x;
+		a.push_back(x);
+	}
+
+	std::sort(a.begin(), a.end());
+	for (auto& x : a)
+		std::cout << x << '\n';
+}
