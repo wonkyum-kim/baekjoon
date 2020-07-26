8266eff39fe07c8dda5705d5821d606d21394819
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-07-26 14:26:08 +0900

    Create 5543.cpp
    정렬을 이용하면 더 쉽게 풀 수 있을 듯

---
+++ 5543.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	vector<int> price;
+
+	for (int i = 0; i < 5; i++)
+	{
+		int a;
+		std::cin >> a;
+		price.push_back(a);
+	}
+
+	int maxdrink = (price[3] < price[4]) ? price[3] : price[4];
+	int maxburger = (price[0] < price[1]) ? (price[0] < price[2]) ? price[0] : price[2] : (price[1] < price[2]) ? price[1] : price[2];
+
+	std::cout << maxdrink + maxburger - 50;
+	return 0;
+}
