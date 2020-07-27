40c51d97dcc6ede9b77215d62965e0ff22ad8362
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-07-27 20:40:20 +0900

    Create 5339.cpp

---
+++ 5339.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string s1 = R"(     /~\)";
+    std::string s2 = R"(    ( oo|)";
+    std::string s3 = R"(    _\=/_)";
+    std::string s4 = R"(   /  _  \)";
+    std::string s5 = R"(  //|/.\|\\)";
+    std::string s6 = R"( ||  \ /  ||)";
+    std::string s7 = R"(============)";
+    std::string s8 = R"(|          |)";
+    std::string s9 = R"(|          |)";
+    std::string s10 = R"(|          |)";
+    
+    std::cout<<s1<<'\n'<<s2<<'\n'<<s3<<'\n'<<s4<<'\n'<<s5<<'\n'<<s6<<'\n'<<s7<<'\n'<<s8<<'\n'<<s9<<'\n'<<s10;
+}
