6232b8e5420c60a6a4e57e0949a5887859c85279
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-08-03 20:56:47 +0900

    max subarray 사용하기

---
+++ 1912.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <limits>
+
+template<typename T>
+int find_maximum_subarray_linear(const std::vector<T>& A)
+{
+	int max_sum = std::numeric_limits<int>::min();		// sentinel value
+	int running_sum = 0;
+
+	for (size_t i = 0; i < A.size(); i++)
+	{
+		if (running_sum <= 0)
+			running_sum = A[i];
+		else
+			running_sum += A[i];
+
+		if (running_sum > max_sum)
+			max_sum = running_sum;
+	}
+	return max_sum;
+}
+
+int main()
+{
+	std::vector<int> v;
+	long long num;
+	std::cin >> num;
+
+	for (auto i = 0; i < num; i++)
+	{
+		int temp;
+		std::cin >> temp;
+		v.push_back(temp);
+	}
+	std::cout << find_maximum_subarray_linear(v);
+}
