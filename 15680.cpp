9ab14d1c3fcc3dd26876b704a97eea25ae77b44b
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-08-15 02:24:26 +0900

    Create 15680.cpp

---
+++ 15680.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    
+    if(n == 1)
+        std::cout << "Leading the Way to the Future";
+    else
+        std::cout << "YONSEI";
+}
