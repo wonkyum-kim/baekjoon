a4d2412993b428e2f9e978fb9d423e4d6bd488e6
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-08-18 23:06:37 +0900

    별 찍기 - 1

--- bronze/3/2438.cpp
+++ bronze/3/2438.cpp
@@ -5,8 +5,7 @@ int main()
 	auto a = 0;
 	std::cin >> a;
 
-	for (int i = 0; i < a; i++)
-	{
+	for (int i = 0; i < a; i++){
 		for (int j = 0; j <= i; j++)
 			std::cout << '*';
 		std::cout << '\n';
