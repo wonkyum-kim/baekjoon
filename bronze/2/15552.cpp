5ffeaa908742022b5cd486da2004ff715f00bd9c
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-08-18 22:59:50 +0900

    빠른 A+B

---
+++ bronze/2/15552.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+	auto T = 0;
+	scanf("%d", &T);
+
+	for(int i = 0; i< T;++i){
+		if (T >= 1 && 1 <= 1000000){
+			int a, b;
+			scanf("%d %d", &a, &b);
+			printf("%d\n", a+b);
+		}
+	}
+	return 0;
+}
