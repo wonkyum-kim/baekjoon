1eee0f3ac015f4a7cbf2da3f60879c15affda69a
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-08-18 22:41:19 +0900

    사칙연산

---
+++ bronze/5/10869.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	auto A = 0, B = 0;
+	cin >> A >> B;
+	cout << A + B << '\n' << A - B << '\n' << A * B << '\n' << A / B << '\n' << A % B;
+	return 0;
+}
