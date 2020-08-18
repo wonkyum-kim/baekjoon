817b7887a3ff6f9b36628f42d7102a226333bf8f
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-08-19 00:16:31 +0900

    와이버스 부릉부릉

---
+++ bronze/5/14645.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int n;
+    int k;
+    int a, b;
+    
+    std::cin >> n >> k;
+    for(auto m = 0; m < n; ++m)
+        std::cin >> a >> b;
+        
+    std::cout << "비와이";
+    
+}
