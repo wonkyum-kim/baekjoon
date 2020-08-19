26be62a5b8c59f02aca0229d36ca43b1c9190544
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-08-19 17:29:52 +0900

    TV 크기

--- bronze/4/1297.cpp
+++ bronze/4/1297.cpp
@@ -6,7 +6,7 @@ int main()
 	int diagonal, rheight, rwidth;
 	std::cin>>diagonal>>rheight>>rwidth;
 	
-	auto ratio = std::sqrt(pow(diagonal, 2) / (pow(rheight,2)+pow(rwidth,2)));
+	auto ratio = std::sqrt(std::pow(diagonal, 2) / (std::pow(rheight,2)+pow(rwidth,2)));
 	int height = rheight * ratio;
 	int width = rwidth * ratio;
 	std::cout << height <<' '<< width;
