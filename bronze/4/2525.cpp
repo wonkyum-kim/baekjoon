083eba86e2572e31f6750cb9a46c8ce848964598
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-08-19 17:34:16 +0900

    오븐 시계

--- bronze/4/2525.cpp
+++ bronze/4/2525.cpp
@@ -7,15 +7,12 @@ int main()
 	int ca = c/60;
 	int cb = c%60;
 	
-	if(cb+b < 60)
-	{
+	if(cb+b < 60) {
 		if(a+ca < 24)
 			std::cout << a + ca << ' ' << cb + b;
 		else
 			std::cout << a + ca - 24 << ' ' << cb + b;
-	}
-	else
-	{
+	} else {
 		if(a+ca+1 < 24)
 			std::cout << a + ca + 1 << ' ' << cb + b - 60;
 		else
