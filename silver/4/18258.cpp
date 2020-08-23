2fc09d81b52e4599f1af50dada30b900da951ca3
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-08-23 20:10:52 +0900

    큐 2

---
+++ silver/4/18258.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <queue>
+#include <string>
+
+int main()
+{
+	std::ios::sync_with_stdio(0);
+	std::cin.tie(0);
+	int test = 0;
+	std::cin >> test;
+
+	std::queue<int> q;
+
+	for (int i = 0; i < test; ++i) {
+		std::string s;
+		std::cin >> s;
+
+		if (s == "push") {
+			int a = 0;
+			std::cin >> a;
+			q.push(a);
+		}
+		else if (s == "pop") {
+			if (!q.empty()) {
+				std::cout << q.front() << '\n';
+				q.pop();
+			}
+			else
+				std::cout << -1 << '\n';
+		}
+		else if (s == "size") {
+			std::cout << q.size() << '\n';
+		}
+		else if (s == "empty") {
+			if (q.empty())
+				std::cout << 1 << '\n';
+			else
+				std::cout << 0 << '\n';
+		}
+		else if (s == "front") {
+			if (q.empty())
+				std::cout << -1 << '\n';
+			else
+				std::cout << q.front() << '\n';
+		}
+		else if (s == "back") {
+			if (q.empty())
+				std::cout << -1 << '\n';
+			else
+				std::cout << q.back() << '\n';
+		}
+	}
+}
