4ff89427b708c4b28307fc06fd989bdfb21c1a3e
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-09-03 00:02:19 +0900

    한수

--- silver/4/1065.cpp
+++ silver/4/1065.cpp
@@ -5,23 +5,22 @@ int main()
 	auto i = 0;
 	std::cin >> i;
 
-	if (i <= 99)
+	if (i <= 99) {
 		std::cout << i;
-	else if (99 < i && i <= 999)
-	{
+	}
+	else if (99 < i && i <= 999) {
 		int count = 99;
-
-		for (auto k = 100; k <= i; k++)
-		{
+		for (auto k = 100; k <= i; k++) {
 			int k1 = k % 10;
 			int k2 = ((k - k1) / 10) % 10;
 			int k3 = k / 100;
-
-			if (k2 - k1 == k3 - k2)
+			if (k2 - k1 == k3 - k2) {
 				count++;
+			}
 		}
 		std::cout << count;
 	}
-	else if (i == 1000)
+	else if (i == 1000) {
 		std::cout << 144;
+	}
 }
