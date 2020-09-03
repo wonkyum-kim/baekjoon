bd5d8f8c7da983b5b81fcd0e787597a2bf6f933a
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-09-03 19:19:08 +0900

    사탕 게임

--- silver/4/3085.cpp
+++ silver/4/3085.cpp
@@ -1,68 +1,74 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <limits>
 
-int check_count(const char arr[][50], const int n) {
+int adjacent_count(const std::vector<std::vector<char>>& candy, const int N)
+{
 	int max_count = 1;
+
 	// 행 단위 검사
-	for (int i = 0; i < n; i++) {
-		int running_sum = 1;
-		for (int j = 1; j < n; j++) {
-			if (arr[i][j] == arr[i][j - 1]) {
-				running_sum++;
-				max_count = std::max(max_count, running_sum);
+	for (auto i = 1; i <= N; ++i) {
+		int running_count = 1;
+		for (auto j = 2; j <= N; ++j) {
+			if (candy[i][j] == candy[i][j - 1]) {
+				running_count++;
+				max_count = std::max(running_count, max_count);
 			}
 			else {
-				max_count = std::max(max_count, running_sum);
-				running_sum = 1;
+				max_count = std::max(running_count, max_count);
+				running_count = 1;
 			}
 		}
 	}
+
 	// 열 단위 검사
-	for (int i = 0; i < n; i++) {
-		int running_sum = 1;
-		for (int j = 1; j < n; j++) {
-			if (arr[j][i] == arr[j - 1][i]) {
-				running_sum++;
-				max_count = std::max(max_count, running_sum);
+	for (auto i = 1; i <= N; ++i) {
+		int running_count = 1;
+		for (auto j = 2; j <= N; ++j) {
+			if (candy[j][i] == candy[j - 1][i]) {
+				running_count++;
+				max_count = std::max(running_count, max_count);
 			}
 			else {
-				max_count = std::max(max_count, running_sum);
-				running_sum = 1;
+				max_count = std::max(running_count, max_count);
+				running_count = 1;
 			}
 		}
 	}
+
 	return max_count;
 }
 
 int main()
 {
-	int n = 0;
-	std::cin >> n;
+	int N = 0;
+	std::cin >> N;
 
-	char arr[50][50];
-	for (auto i = 0; i < n; ++i) {
-		for (auto j = 0; j < n; ++j) {
-			std::cin >> arr[i][j];
+	std::vector<std::vector<char>> candy(N + 1, std::vector<char>(N + 1));
+	for (auto i = 1; i <= N; ++i) {
+		for (auto j = 1; j <= N; ++j) {
+			std::cin >> candy[i][j];
 		}
 	}
 
 	int result = std::numeric_limits<int>::min();
 
-	for (auto i = 0; i < n; ++i) {
-		for (auto j = 0; j < n - 1; ++j) {
-			// 오른쪽 교환 후 원상 복구
-			std::swap(arr[i][j], arr[i][j + 1]);
-			result = std::max(check_count(arr, n), result);
-			std::swap(arr[i][j], arr[i][j + 1]);
+	// 오른쪽 교환 이후 원상 복구
+	for (auto i = 1; i <= N; ++i) {
+		for (auto j = 1; j <= N - 1; ++j) {
+			std::swap(candy[i][j], candy[i][j + 1]);
+			result = std::max(result, adjacent_count(candy, N));
+			std::swap(candy[i][j], candy[i][j + 1]);
 		}
 	}
 
-	for (auto i = 0; i < n - 1; ++i) {
-		for (auto j = 0; j < n; ++j) {
-			// 아래쪽 교환 후 원상 복구
-			std::swap(arr[i][j], arr[i + 1][j]);
-			result = std::max(check_count(arr, n), result);
-			std::swap(arr[i][j], arr[i + 1][j]);
+	// 아래쪽 교환 이후 원상 복구
+	for (auto i = 1; i <= N - 1; ++i) {
+		for (auto j = 1; j <= N; ++j) {
+			std::swap(candy[i][j], candy[i + 1][j]);
+			result = std::max(result, adjacent_count(candy, N));
+			std::swap(candy[i][j], candy[i + 1][j]);
 		}
 	}
 
