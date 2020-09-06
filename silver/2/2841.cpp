0b5fffdad93cad6f38f5f59d9904f509d28b7c2d
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-09-06 23:55:20 +0900

    외계인의 기타 연주

--- silver/2/2841.cpp
+++ silver/2/2841.cpp
@@ -6,160 +6,55 @@
 
 void count_fingering(const std::vector<std::pair<int, int>>& melody) {
 
+	std::vector<std::stack<int>> line;
+	std::stack<int> dummy;
 	std::stack<int> line1;
 	std::stack<int> line2;
 	std::stack<int> line3;
 	std::stack<int> line4;
 	std::stack<int> line5;
 	std::stack<int> line6;
-
+	line.push_back(dummy);
+	line.push_back(line1);
+	line.push_back(line2);
+	line.push_back(line3);
+	line.push_back(line4);
+	line.push_back(line5);
+	line.push_back(line6);
 	int count = 0;
 
 	for (auto x : melody) {
-
-		if (x.first == 1) {
-			if (line1.empty()) {
-				line1.push(x.second);
-				count++;
-			}
-			else {
-				while(line1.top() > x.second){
-					line1.pop();
-					count++;
-					if (line1.empty())
-						break;
-				}
-				if (!line1.empty()) {
-					if (line1.top() < x.second) {
-						line1.push(x.second);
-						count++;
-					}
-				}
-				else {
-					line1.push(x.second);
-					count++;
-				}
-			}
+		// 비어있다면
+		if (line[x.first].empty()) {
+			// 새로 누른다.
+			line[x.first].push(x.second);
+			count++;
 		}
-		else if (x.first == 2) {
-			if (line2.empty()) {
-				line2.push(x.second);
-				count++;
-			}
-			else {
-				while (line2.top() > x.second) {
-					line2.pop();
+		// 비어있지 않다면
+		else {
+			// 더 작은 프렛을 눌러야 한다면
+			if (line[x.first].top() > x.second) {
+				while (true) {
+					line[x.first].pop();
 					count++;
-					if (line2.empty())
+					// 스택이 비거나 같거나 더 커질 때까지 pop
+					if (line[x.first].empty() || line[x.first].top() <= x.second) {
 						break;
-				}
-				if (!line2.empty()) {
-					if (line2.top() < x.second) {
-						line2.push(x.second);
-						count++;
 					}
 				}
-				else {
-					line2.push(x.second);
+				// 스택이 비었거나 이미 누른 상태가 아니라면
+				if (line[x.first].empty() || line[x.first].top() != x.second) {
+					line[x.first].push(x.second);
 					count++;
 				}
 			}
-		}
-		else if (x.first == 3) {
-			if (line3.empty()) {
-				line3.push(x.second);
+			// 더 큰 프렛을 눌러야 한다면
+			else if (line[x.first].top() < x.second) {
+				// 새로 누른다.
+				line[x.first].push(x.second);
 				count++;
 			}
-			else {
-				while (line3.top() > x.second) {
-					line3.pop();
-					count++;
-					if (line3.empty())
-						break;
-				}
-				if (!line3.empty()) {
-					if (line3.top() < x.second) {
-						line3.push(x.second);
-						count++;
-					}
-				}
-				else {
-					line3.push(x.second);
-					count++;
-				}
-			}
-		}
-		else if (x.first == 4) {
-			if (line4.empty()) {
-				line4.push(x.second);
-				count++;
-			}
-			else {
-				while (line4.top() > x.second) {
-					line4.pop();
-					count++;
-					if (line4.empty())
-						break;
-				}
-				if (!line4.empty()) {
-					if (line4.top() < x.second) {
-						line4.push(x.second);
-						count++;
-					}
-				}
-				else {
-					line4.push(x.second);
-					count++;
-				}
-			}
-		}
-		else if (x.first == 5) {
-			if (line5.empty()) {
-				line5.push(x.second);
-				count++;
-			}
-			else {
-				while (line5.top() > x.second) {
-					line5.pop();
-					count++;
-					if (line5.empty())
-						break;
-				}
-				if (!line5.empty()) {
-					if (line5.top() < x.second) {
-						line5.push(x.second);
-						count++;
-					}
-				}
-				else {
-					line5.push(x.second);
-					count++;
-				}
-			}
-		}
-		else if (x.first == 6) {
-			if (line6.empty()) {
-				line6.push(x.second);
-				count++;
-			}
-			else {
-				while (line6.top() > x.second) {
-					line6.pop();
-					count++;
-					if (line6.empty())
-						break;
-				}
-				if (!line6.empty()) {
-					if (line6.top() < x.second) {
-						line6.push(x.second);
-						count++;
-					}
-				}
-				else {
-					line6.push(x.second);
-					count++;
-				}
-			}
+			// 이미 누른 프렛이라면 아무것도 하지 않는다.
 		}
 	}
 	std::cout << count;
@@ -179,6 +74,5 @@ int main()
 		std::cin >> a >> b;
 		melody.push_back({ a,b });
 	}
-
 	count_fingering(melody);
 }
