32eadaeed006099ced0545847d45a187cf91fe3c
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-09-10 01:52:44 +0900

    풍선 터뜨리기

--- silver/3/2346.cpp
+++ silver/3/2346.cpp
@@ -1,54 +1,89 @@
-#include <iostream>
+
+#include<list>
+#include<iostream>
 #include <utility>
-#include <deque>
+
+template<typename T>
+struct CircularDeque {
+	std::list<T> cd;
+
+	// front access
+	T front() {
+		return cd.front();
+	}
+	// back access
+	T back() {
+		return cd.back();
+	}
+	// push_front
+	void push_front(const T& key) {
+		cd.push_front(key);
+	}
+	// push_back
+	void push_back(const T& key) {
+		cd.push_back(key);
+	}
+	// pop_front
+	void pop_front() {
+		cd.pop_front();
+	}
+	// pop_back
+	void pop_back() {
+		cd.pop_front();
+	}
+	// next access
+	T next() {
+		// shift left
+		T temp = cd.front();
+		cd.pop_front();
+		cd.push_back(temp);
+		return cd.front();
+	}
+	// prev access
+	T prev() {
+		// shift right
+		T temp = cd.back();
+		cd.pop_back();
+		cd.push_front(temp);
+		return cd.front();
+	}
+	// empty
+	bool empty() {
+		return cd.empty() ? true : false;
+	}
+};
 
 int main()
 {
 	int N = 0;
 	std::cin >> N;
-	std::deque<std::pair<int, int>> dq;
+	CircularDeque<std::pair<int, int>> cd;
 
 	for (auto i = 1; i <= N; ++i) {
 		int temp = 0;
 		std::cin >> temp;
-		dq.push_back({ temp, i });
+		cd.push_back({ temp, i });
 	}
 
 	while (true) {
-		if (dq.empty()) {
+		std::pair<int, int> ballon = cd.front();
+		std::cout << ballon.second << ' ';
+		cd.pop_front();
+		int loop = ballon.first;
+		if (cd.empty()) {
 			break;
 		}
-		else {
-			std::pair<int, int> ballon = dq.front();
-			std::cout << ballon.second << ' ';
-			dq.pop_front();
 
-			// shift left
-			if (ballon.first > 0) {
-				for (auto i = 1; i < ballon.first; ++i) {
-					if (!dq.empty()) {
-						std::pair<int, int> temp = dq.front();
-						dq.pop_front();
-						dq.push_back(temp);
-					}
-				}
-				if (!dq.empty()) {
-					ballon = dq.front();
-				}
-			}
-			// shift right
-			else {
-				for (auto i = 1; i <= -1 * ballon.first; ++i) {
-					if (!dq.empty()) {
-						std::pair<int, int> temp = dq.back();
-						dq.pop_back();
-						dq.push_front(temp);
-					}
-				}
-				if (!dq.empty()) {
-					ballon = dq.front();
-				}
+		if (ballon.first > 0) {
+			for (auto i = 1; i < loop; ++i) {
+				ballon = cd.next();
 			}
 		}
+		else if (ballon.first < 0) {
+			for (auto i = 1; i <= -1 * loop; ++i) {
+				ballon = cd.prev();
+			}
+		}
+
 	}
 }
