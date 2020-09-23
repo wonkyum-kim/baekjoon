ade046c8f7bd6b6f1d3ac9e0108cfd2012d1e8ac
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-09-23 18:29:58 +0900

    숫자 야구

--- silver/5/(incomplete)2503.cpp
+++ silver/5/(incomplete)2503.cpp
@@ -1,62 +1,63 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
-using namespace std;
-int S, B, T;
-string s;
-vector<string> v;
-int check[1000];
+int main()
+{
+	int test = 0;
+	std::cin >> test;
 
-
-int main() {
-	cin >> T;
-	for (int i = 1; i <= 9; i++) {
-		for (int j = 1; j <= 9; j++) {
-			if (j != i) {
-				for (int k = 1; k <= 9; k++) {
-					if (k != i && k != j) {
-						check[i * 100 + j * 10 + k] = 1;
-					}
+	std::vector<bool> baseball(1000, false);
+	for (auto i = 1; i <= 9; ++i) {
+		for (auto j = 1; j <= 9; ++j) {
+			for (auto k = 1; k <= 9; ++k) {
+				if (i != j && i != k && j != k) {
+					baseball[i * 100 + j * 10 + k] = true;
 				}
 			}
 		}
 	}
 
-	for (int i = 0; i < T; i++) {
-		cin >> s >> S >> B;
-		for (int i = 123; i <= 999; i++) {
-			if (check[i] == 1) {
-				string a = to_string(i);
-				int str = 0;
-				int ball = 0;
-				for (int i = 0; i < 3; i++) {
-					if (a[i] == s[i]) {
-						str++;
-					}
+	for (auto i = 0; i < test; ++i) {
+		int thrown = 0;
+		int strike = 0;
+		int ball = 0;
+		std::cin >> thrown >> strike >> ball;
+		int thrown_a = thrown / 100;
+		int thrown_c = thrown % 10;
+		int thrown_b = (thrown - thrown_c - thrown_a * 100) / 10;
+		for (auto j = 123; j <= 999; ++j) {
+			if (baseball[j] == true) {
+				int check_a = j / 100;
+				int check_c = j % 10;
+				int check_b = (j - check_c - check_a * 100) / 10;
+				int temp_strike = 0;
+				int temp_ball = 0;
+				if (thrown_a == check_a) {
+					temp_strike++;
 				}
-					if (a[0] == s[1] || a[0] == s[2]) {
-						ball++;
-					}
-					if (a[1] == s[0] || a[1] == s[2]) {
-						ball++;
-					}
-					if (a[2] == s[0] || a[2] == s[1]) {
-						ball++;
-					}
-
-					if (str != S || ball != B) {
-						check[i] = 0;
-					}
+				if (thrown_b == check_b) {
+					temp_strike++;
+				}
+				if (thrown_c == check_c) {
+					temp_strike++;
+				}
+				if (thrown_a == check_b || thrown_a == check_c) {
+					temp_ball++;
+				}
+				if (thrown_b == check_a || thrown_b == check_c) {
+					temp_ball++;
+				}
+				if (thrown_c == check_a || thrown_c == check_b) {
+					temp_ball++;
+				}
+				if (temp_ball != ball || temp_strike != strike) {
+					baseball[j] = false;
+				}
 			}
 		}
 	}
-
-	int cnt = 0;
-	for (int i = 123; i <= 999; i++) {
-		if (check[i] == 1) {
-			cnt++;
-		}
-	}
-	cout << cnt;
+	int possible = std::count(baseball.begin() + 123, baseball.end(), true);
+	std::cout << possible;
 }
