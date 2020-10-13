138ee094a06c6c6c62769fe1c13192bf3eb7b818
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-10-13 10:30:14 +0900

    1로 만들기

--- silver/3/1463.cpp
+++ silver/3/1463.cpp
@@ -3,25 +3,18 @@
 #include <limits>
 #include <algorithm>
 
-/*
-점화식 f(N)
-1. (N == 1)	0
-2. (N%3==0) f(N/3)+1
-3. (N%2==0) f(N/2)+1
-4. (else) f(N-1) + 1
-*/
 
 void dp(const int n)
 {
-	std::vector<int> r(n + 1, std::numeric_limits<int>::max());
+	std::vector<int> r(n + 1, 0);
 	r[1] = 0;	// base case
-	for (auto i = 1; i < n; ++i) {
-		r[i + 1] = std::min(r[i + 1], r[i] + 1);
-		if (i * 2 <= n) {
-			r[i * 2] = std::min(r[i * 2], r[i] + 1);
+	for (auto i = 2; i <= n; ++i) {
+		r[i] = r[i - 1] + 1;
+		if (i % 2 == 0) {
+			r[i] = std::min(r[i / 2] + 1, r[i]);
 		}
-		if (i * 3 <= n) {
-			r[i * 3] = std::min(r[i * 3], r[i] + 1);
+		if (i % 3 == 0) {
+			r[i] = std::min(r[i / 3] + 1, r[i]);
 		}
 	}
 	std::cout << r[n];
