3f3f1d5e5c4b216055b32fdfc6443350da6986ee
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-11-13 23:44:58 +0900

    가장 긴 증가하는 부분 수열 5

---
+++ gold/1/14003.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+void backtrace(int n, int length, std::vector<long long>& P, std::vector<long long>& A)
+{
+	if (n == 0) {
+		return;
+	}
+	if (P[n] == length) {
+		backtrace(n - 1, length - 1, P, A);
+		std::cout << A[n] << ' ';
+	}
+	else {
+		backtrace(n - 1, length, P, A);
+	}
+}
+
+int main()
+{
+	int n = 0;
+	std::cin >> n;
+	std::vector<long long> A(n + 1);
+	for (auto i = 1; i <= n; ++i) {
+		std::cin >> A[i];
+	}
+	std::vector<long long> L;
+	std::vector<long long> P(n + 1);
+
+	for (auto i = 1; i <= n; ++i) {
+		if (L.empty() || L.back() < A[i]) {
+			L.push_back(A[i]);
+			P[i] = L.size();
+		}
+		else {
+			auto lower = std::lower_bound(L.begin(), L.end(), A[i]);
+			auto idx = std::distance(L.begin(), lower);
+			L[idx] = A[i];
+			P[i] = std::distance(L.begin(), lower) + 1;
+		}
+	}
+
+	std::cout << L.size() << '\n';
+	backtrace(n, L.size(), P, A);
+}
