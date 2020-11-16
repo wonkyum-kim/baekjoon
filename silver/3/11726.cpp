87caa3a91da41eb2a65d0f2ea6e4b92cff93733c
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-11-16 09:52:28 +0900

    2 x n 타일링

---
+++ silver/3/11726.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <numeric>
+
+void tile(int n)
+{
+	if (n == 1) {
+		std::cout << 1;
+		return;
+	}
+	else if (n == 2) {
+		std::cout << 2;
+		return;
+	}
+	else {
+		std::vector<long long> dp(n + 1);
+		dp[1] = 1;
+		dp[2] = 2;
+		for (auto i = 3; i <= n; ++i) {
+			dp[i] = dp[i - 2] % 10'007 + dp[i - 1] % 10'007;
+		}
+		std::cout << dp[n]%10'007;
+
+	}
+}
+
+int main()
+{
+	int n = 0;
+	std::cin >> n;
+	tile(n);
+}
