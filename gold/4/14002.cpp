acd87c5d2580c55289e511421cebc1562e9e41ab
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-12-19 18:42:10 +0900

    가장 긴 증가하는 부분 수열 4

--- gold/4/14002.cpp
+++ gold/4/14002.cpp
@@ -1,47 +1,51 @@
-#include <vector>
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <stack>
 
-void backtrace(int n, int length, std::vector<long long>& P, std::vector<long long>& A)
+std::vector<int> lis(std::vector<int>& A, int n)
 {
-	if (n == 0) {
-		return;
+	std::vector<int> dp(n, 1);
+	for (auto i = 0; i < n; ++i) {
+		for (auto j = 0; j < i; ++j) {
+			if (A[j] < A[i]) {
+				dp[i] = std::max(dp[i], dp[j] + 1);
+			}
+		}
 	}
-	if (P[n] == length) {
-		backtrace(n - 1, length - 1, P, A);
-		std::cout << A[n] << ' ';
-	}
-	else {
-		backtrace(n - 1, length, P, A);
-	}
+	return dp;
 }
 
+void print_lis(int length, std::vector<int>& dp, std::vector<int>& A)
+{
+	std::stack<int> s;
+	for (int i = dp.size() - 1; i >= 0; --i) {
+		if (dp[i] == length) {
+			s.push(A[i]);
+			length--;
+			if (length == 0) {
+				break;
+			}
+		}
+	}
+	while (!s.empty()) {
+		std::cout << s.top() << ' ';
+		s.pop();
+	}
+}
 
 int main()
 {
 	int n = 0;
 	std::cin >> n;
-	std::vector<long long> A(n + 1);
-	for (auto i = 1; i <= n; ++i) {
+
+	std::vector<int> A(n);
+	for (auto i = 0; i < n; ++i) {
 		std::cin >> A[i];
 	}
-	std::vector<long long> L;
-	std::vector<long long> P(n + 1);
 
-	for (auto i = 1; i <= n; ++i) {
-		if (L.empty() || L.back() < A[i]) {
-			L.push_back(A[i]);
-			P[i] = L.size();
-		}
-		else {
-			auto lower = std::lower_bound(L.begin(), L.end(), A[i]);
-			auto idx = std::distance(L.begin(), lower);
-			L[idx] = A[i];
-			P[i] = std::distance(L.begin(), lower) + 1;
-		}
-	}
-
-	std::cout << L.size() << '\n';
-	backtrace(n, L.size(), P, A);
+	auto dp = lis(A, n);
+	int length = *std::max_element(dp.begin(), dp.end());
+	std::cout << length << '\n';
+	print_lis(length, dp, A);
 }
-	
