37b8fbdb4e8847231cd87bd1284e66df332fd6ab
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-12-19 18:57:54 +0900

    가장 긴 증가하는 부분 수열 5

--- gold/1/14003.cpp
+++ gold/1/14003.cpp
@@ -1,33 +1,13 @@
-#include <vector>
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <stack>
 
-void backtrace(int n, int length, std::vector<long long>& P, std::vector<long long>& A)
+std::pair<long long, std::vector<long long>> lis(std::vector<long long>& A, int n)
 {
-	if (n == 0) {
-		return;
-	}
-	if (P[n] == length) {
-		backtrace(n - 1, length - 1, P, A);
-		std::cout << A[n] << ' ';
-	}
-	else {
-		backtrace(n - 1, length, P, A);
-	}
-}
-
-int main()
-{
-	int n = 0;
-	std::cin >> n;
-	std::vector<long long> A(n + 1);
-	for (auto i = 1; i <= n; ++i) {
-		std::cin >> A[i];
-	}
 	std::vector<long long> L;
-	std::vector<long long> P(n + 1);
-
-	for (auto i = 1; i <= n; ++i) {
+	std::vector<long long> P(n);
+	for (auto i = 0; i < n; ++i) {
 		if (L.empty() || L.back() < A[i]) {
 			L.push_back(A[i]);
 			P[i] = L.size();
@@ -36,10 +16,41 @@ int main()
 			auto lower = std::lower_bound(L.begin(), L.end(), A[i]);
 			auto idx = std::distance(L.begin(), lower);
 			L[idx] = A[i];
-			P[i] = std::distance(L.begin(), lower) + 1;
+			P[i] = idx + 1;
 		}
 	}
+	return { L.size(), P };
+}
 
-	std::cout << L.size() << '\n';
-	backtrace(n, L.size(), P, A);
+void print_lis(long long length, std::vector<long long>& dp, std::vector<long long>& A)
+{
+	std::stack<long long> s;
+	for (long long i = dp.size() - 1; i >= 0; --i) {
+		if (dp[i] == length) {
+			s.push(A[i]);
+			length--;
+			if (length == 0) {
+				break;
+			}
+		}
+	}
+	while (!s.empty()) {
+		std::cout << s.top() << ' ';
+		s.pop();
+	}
+}
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(0);
+	int n = 0;
+	std::cin >> n;
+	std::vector<long long> A(n);
+	for (auto i = 0; i < n; ++i) {
+		std::cin >> A[i];
+	}
+	auto [a, b] = lis(A, n);
+	std::cout << a << '\n';
+	print_lis(a, b, A);
 }
