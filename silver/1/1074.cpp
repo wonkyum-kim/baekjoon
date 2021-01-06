9428a642a8142ecb8ecf6bc4375fa9c42989d4c9
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2021-01-06 15:33:03 +0900

    Z

--- silver/1/1074.cpp
+++ silver/1/1074.cpp
@@ -1,44 +1,47 @@
 #include<iostream>
 #include <vector>
-#include <algorithm>
 #include <cmath>
 
-int count = 0;
-
-void visit_Z(const int r, const int c, const int row, int x, int y)
+int dc(int n, int r, int c)
 {
-	// base case
-	if (row == 2) {
-		if (x == r && y == c) {
-			std::cout << count;
-		}
-		count++;
-		if (x == r && y + 1 == c) {
-			std::cout << count;
-		}
-		count++;
-		if (x + 1 == r && y == c) {
-			std::cout << count;
-		}
-		count++;
-		if (x + 1 == r && y + 1 == c) {
-			std::cout << count;
-		}
-		count++;
-		return;
-	}
+	// 4분할 했을 때 한 변의 길이
+	int size = std::pow(2, n - 1);
 
-	visit_Z(r, c, row / 2, x, y);
-	visit_Z(r, c, row / 2, x, y + row / 2);
-	visit_Z(r, c, row / 2, x + row / 2, y);
-	visit_Z(r, c, row / 2, x + row / 2, y + row / 2);
+	if (n == 0) {
+		return 0;
+	}
+	// 사각형 상단에 위치
+	if (r < size) {
+		// 왼쪽
+		if (c < size) {
+			return dc(n-1,r,c);
+		}
+		// 오른쪽
+		else {
+			return size * size + dc(n - 1, r, c - size);
+		}
+	}
+	// 사각형 하단에 위치
+	else {
+		// 왼쪽
+		if (c < size) {
+			return size * size * 2 +  dc(n - 1, r - size, c);
+		}
+		// 오른쪽
+		else {
+			return size * size * 3 + dc(n - 1, r - size, c - size);
+		}
+	}
 }
+
 
 int main()
 {
-	int N = 0;
-	int r = 0, c = 0;
-	std::cin >> N >> r >> c;
-	int row = std::pow(2, N);
-	visit_Z(r, c, row, 0, 0);
+	std::ios_base::sync_with_stdio(0);
+	std::cin.tie(0);
+	int n = 0;
+	int r = 0;
+	int c = 0;
+	std::cin >> n >> r >> c;
+	std::cout << dc(n, r, c);
 }
